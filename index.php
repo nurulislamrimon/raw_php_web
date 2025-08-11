a282ea2fab2a126585e07ab207b5d6a3e16e42e0
nurulislamrimon <nurulislamrimon@gmail.com>
2025-08-11 13:10:16 +0600

    STYLE_UPDATED

--- index.php
+++ index.php
@@ -5,6 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <style>
+        body {
+            background-color: #f0f0f0;
+            font-family: Arial, sans-serif;
+            color: #333;
+            text-align: center;
+            padding: 50px;
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 
 <body>
