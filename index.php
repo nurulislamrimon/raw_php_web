bcbea9db3f541394a00dce6eb731ca1813acb493
nurulislamrimon <nurulislamrimon@gmail.com>
2025-08-11 22:47:15 +0600

    DB connected

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
 
     <?php
 
-    $uri = $_SERVER["REQUEST_URI"];
+    require_once("./router.php");
+
+    $dsn = "mysql:host=localhost;port=3306;user=root;password=Nurul123;dbname=bdentrepreneur;charset=utf8mb4";
+
+    $pdo = new PDO($dsn);
 
 
-    $routes = [
-        "/" => "./pages/home.php",
-        "/contact" => "./pages/contact.php",
-        "/pricing" => "./pages/pricing.php",
-        "/services" => "./pages/services.php",
-    ];
+    $statement = $pdo->prepare("select * from administrator");
 
+    $statement->execute();
 
-    if (array_key_exists($uri, $routes)) {
-        require_once($routes[$uri]);
-    } else {
-        http_response_code(404);
-        require_once("./pages/notfound.php");
-    }
+    $users = $statement->fetchAll();
+
+    echo var_dump($users);
---
+++ router.php
@@ -0,0 +1,20 @@
+
+    <?php
+
+    $uri = $_SERVER["REQUEST_URI"];
+
+
+    $routes = [
+        "/" => "./pages/home.php",
+        "/contact" => "./pages/contact.php",
+        "/pricing" => "./pages/pricing.php",
+        "/services" => "./pages/services.php",
+    ];
+
+
+    if (array_key_exists($uri, $routes)) {
+        require_once($routes[$uri]);
+    } else {
+        http_response_code(404);
+        require_once("./pages/notfound.php");
+    }
