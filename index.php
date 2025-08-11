343ee6d143cead83b9fd596fb128769f02482961
nurulislamrimon <nurulislamrimon@gmail.com>
2025-08-11 17:44:19 +0600

    VIEW_SEPARATED

--- index.php
+++ index.php
@@ -1,29 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <style>
-        body {
-            background-color: #f0f0f0;
-            font-family: Arial, sans-serif;
-            color: #333;
-            text-align: center;
-            padding: 50px;
-            display: grid;
-            place-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: sans-serif;
-        }
-    </style>
-</head>
-
-<body>
-    <h1>Recommended Books</h1>
 
     <?php
     $books = [
@@ -32,38 +6,6 @@
         "Pride and Prejudice" => "Jane Austen",
         "The Great Gatsby" => "F. Scott Fitzgerald",
         "The Great" => "F. Scott Fitzgerald",
-    ]
-    ?>
-
-    <?php foreach (
-        $books as $title => $author
-    ): ?>
-        <div>
-            <h2><?php echo $title . "<sup><small>tm</small></sup>"; ?></h2>
-            <p>by <?php echo $author; ?></p>
-        </div>
-    <?php endforeach; ?>
-
-    <?php
-    $gratitude = [
-        "Thanks ",
-        "Mr."
     ];
 
-    function simpleFn()
-    {
-        return "<h2>Gratitude</h2>";
-    }
-
-    ?>
-
-    <p>
-        <?php foreach ($gratitude as $w): ?>
-            <span><?= $w; ?></span>
-        <?php endforeach ?>
-    </p>
-
-    <p><?= simpleFn() ?></p>
-</body>
-
-</html>
+    include_once "./index.view.php";
---
+++ index.view.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        body {
+            background-color: #f0f0f0;
+            font-family: Arial, sans-serif;
+            color: #333;
+            text-align: center;
+            padding: 50px;
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Recommended Books</h1>
+
+
+    <?php foreach (
+        $books as $title => $author
+    ): ?>
+        <div>
+            <h2><?php echo $title . "<sup><small>tm</small></sup>"; ?></h2>
+            <p>by <?php echo $author; ?></p>
+        </div>
+    <?php endforeach; ?>
+
+    <?php
+    $gratitude = [
+        "Thanks ",
+        "Mr."
+    ];
+
+    function simpleFn()
+    {
+        return "<h2>Gratitude</h2>";
+    }
+
+    ?>
+
+    <p>
+        <?php foreach ($gratitude as $w): ?>
+            <span><?= $w; ?></span>
+        <?php endforeach ?>
+    </p>
+
+    <p><?= simpleFn() ?></p>
+</body>
+
+</html>
