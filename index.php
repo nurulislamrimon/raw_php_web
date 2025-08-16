fe9c685ca51ece89f97577a01f2183b39910f201
nurulislamrimon <nurulislamrimon@gmail.com>
2025-08-16 19:32:49 +0600

    SQL injection prevented with associative array : Laracasts lession-20

--- index.php
+++ index.php
@@ -9,7 +9,9 @@
     $email = $_GET['email'];
 
     $db = new Database($config['database'], $config['db_username'], $config['db_password']);
-    $query = "select * from user where email = ?";
-    $data = $db->query($query, [$email])->fetch();
+
+    $query = "select * from user where email = :email";
+
+    $data = $db->query($query, [':email' => $email])->fetchAll();
 
     logger($data);
