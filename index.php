937b8f598f3f7eb2cd7241e2abaa572213b33ab3
nurulislamrimon <nurulislamrimon@gmail.com>
2025-08-16 12:52:54 +0600

    simple data retrive done

--- index.php
+++ index.php
@@ -2,16 +2,17 @@
     <?php
 
     require_once("./router.php");
+    require_once("./utils/logger.php");
 
-    $dsn = "mysql:host=localhost;port=3306;user=root;password=Nurul123;dbname=bdentrepreneur;charset=utf8mb4";
+    $dsn = "mysql:host=localhost;port=3306;user=root;password=Nurul123;dbname=php_practice;charset=utf8mb4";
 
     $pdo = new PDO($dsn);
 
 
-    $statement = $pdo->prepare("select * from administrator");
+    $statement = $pdo->prepare("select * from user where email='ni@gmail.com'");
 
     $statement->execute();
 
-    $users = $statement->fetchAll();
+    $users = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-    echo var_dump($users);
+    logger($users);
---
+++ utils/logger.php
@@ -0,0 +1,9 @@
+<?php
+
+function logger($data)
+{
+    echo "<pre>";
+    echo var_dump($data);
+    echo "<pre>";
+    die();
+}
