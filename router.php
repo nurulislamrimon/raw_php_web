05d48cf74008992f18b75c8601f2890a2daa8e68
nurulislamrimon <nurulislamrimon@gmail.com>
2025-08-18 10:26:02 +0600

    page renamed to controllers

--- router.php
+++ router.php
@@ -5,16 +5,20 @@
 
 
     $routes = [
-        "/" => "./pages/home.php",
-        "/contact" => "./pages/contact.php",
-        "/pricing" => "./pages/pricing.php",
-        "/services" => "./pages/services.php",
+        "/" => "./controllers/home.php",
+        "/contact" => "./controllers/contact.php",
+        "/pricing" => "./controllers/pricing.php",
+        "/services" => "./controllers/services.php",
     ];
 
-
-    if (array_key_exists($uri, $routes)) {
-        require_once($routes[$uri]);
-    } else {
-        http_response_code(404);
-        require_once("./pages/notfound.php");
+    function routeToController($uri, $routes)
+    {
+        if (array_key_exists($uri, $routes)) {
+            require_once($routes[$uri]);
+        } else {
+            http_response_code(404);
+            require_once("./controllers/notfound.php");
+        }
     }
+
+    routeToController($uri, $routes);
